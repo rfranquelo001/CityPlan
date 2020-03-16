3fd6cf8960d62544c4e831e706d9e7dfd3a31c84
Raquel-Franquelo <trf.12.svb@gmail.com>
2020-03-16 17:53:51 +0100

    servlet login

---
+++ CityPlanGit/src/controlador/ServletLogIn.java
@@ -0,0 +1,71 @@
+package controlador;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Vista.Utilidades;
+import modelo.Cliente;
+import modelo.Comerciantes;
+
+/**
+ * Servlet implementation class ServletLogIn
+ */
+@WebServlet("/ServletLogIn")
+public class ServletLogIn extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletLogIn() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter pwGet=response.getWriter();
+		pwGet.println(Utilidades.DOCTYPE);
+		pwGet.println(Utilidades.headConTitleStyle("Error, solo se puede acceder por POST", "estiloCityPlan.css"));
+		response.setContentType("text/html;charset=UTF-8");
+		pwGet.println("<body><h2>Este servlet sólo atiende peticiones con POST<h2>");
+		pwGet.println(Utilidades.FIN);
+		pwGet.close();
+		
+		doPost(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter pw=response.getWriter();
+		String tipoUsuario;
+		String usuario;
+		String password;
+		
+		//miro si es Comerciante ó cliente:
+			tipoUsuario= request.getParameter("tipoUsuario");
+			if(tipoUsuario.equals("cliente")) {
+				Cliente cliente=new Cliente();
+				//>>¿COMO HAGO? creo un b¡objeto y busco el nombre en la BBDD y despues comparo con el password??
+				//comparar datos del cliente
+				
+				
+			}else {
+				Comerciantes comerciante=new Comerciantes();
+				//comparar datos del comerciante
+				
+			}
+	}
+
+}

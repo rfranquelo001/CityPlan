67ba224a8327636adc15052f2462c622fcfbbfca
Raquel-Franquelo <trf.12.svb@gmail.com>
2020-05-03 06:08:44 +0200

    Añadido mensaje error login, metodo listaEventos Rest

--- CityPlanGit/src/bl/RestProvider.java
+++ CityPlanGit/src/bl/RestProvider.java
@@ -1,9 +1,13 @@
 package bl;
 
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import dl.Evento;
 
 @Path("/restProvider")
 public class RestProvider {
@@ -11,9 +15,9 @@ public class RestProvider {
 	@GET
 	@Path("/listaEventos")
 	@Produces(MediaType.APPLICATION_XML)
-	public Agenda listaEventos() {
+	public List<Evento> listaEventos() {
 		Agenda listaEventos = new Agenda();
-		return listaEventos;
+		return listaEventos.getAgenda();
 	}
 
 }
--- CityPlanGit/src/pl/VistaPagUsuario.java
+++ CityPlanGit/src/pl/VistaPagUsuario.java
@@ -2,28 +2,20 @@ package pl;
 
 import java.io.PrintWriter;
 
-public class VistaPagUsuario implements InterfazVista{
+public class VistaPagUsuario implements InterfazVista {
 
 	@Override
 	public void imprime(PrintWriter pw) {
 		// TODO Auto-generated method stub
-		
-		
+
 	}
+
 	public void verPagFormularioOpinion(PrintWriter pw) {
-		String codigoScript=
-				"function msg(){\n" + 
-				"	alert(\"¡Gracias por tu valoración!\");\n" + 
-				"}\n" + 
-				"\n" + 
-				"function msgOpinion(){\n" + 
-				"	if(document.getElementById(\"message\").value==\"\"){ \n" + 
-				"		alert(\"Por favor escribe una breve opinión\");\n" + 
-				"	}else{\n" + 
-				"		alert(\"¡Gracias por tu opinión!\");\n" + 
-				"	}\n" + 
-				"}";
-		String contenidoFormOpinion= "";
+		String codigoScript = "function msg(){\n" + "	alert(\"¡Gracias por tu valoración!\");\n" + "}\n" + "\n"
+				+ "function msgOpinion(){\n" + "	if(document.getElementById(\"message\").value==\"\"){ \n"
+				+ "		alert(\"Por favor escribe una breve opinión\");\n" + "	}else{\n"
+				+ "		alert(\"¡Gracias por tu opinión!\");\n" + "	}\n" + "}";
+		String contenidoFormOpinion = "";
 		pw.println(Utilidades.DOCTYPE);
 		pw.println(Utilidades.headConTitleStyle("Error LogIn", " "));
 		pw.println(Utilidades.headerConMenu());
@@ -32,13 +24,19 @@ public class VistaPagUsuario implements InterfazVista{
 		pw.println(Utilidades.bodyContainer(contenidoFormOpinion));
 		pw.println(Utilidades.bodyFiltrosyFooter());
 		pw.println(Utilidades.FIN);
-		
+
 	}
+
 	public void verErrorLogIn(PrintWriter pw, String contenido) {
+		String errorLogIn = "<p>ERROR: Log In incorrecto</p><button onclick=\"goBack()\">Volver</button>";
+		String codigoGoBack = "<script>\n" + "function goBack() {\n" + "  window.history.back();\n" + "}\n"
+				+ "</script>";
 		pw.println(Utilidades.DOCTYPE);
 		pw.println(Utilidades.headConTitleStyle("Error LogIn", " "));
+		pw.println(Utilidades.scriptHead(codigoGoBack));
 		pw.println(Utilidades.headerConMenu());
-		//pw.println(Utilidades.bodyContainer(contenido));>>FALTA RELLENAR CON EL MENSAJE DE ERROR DE ACCESO Y UN BOTON DE VUELTA ATRAS!!
+		pw.println(Utilidades.bodyContainer(errorLogIn));
+		// MENSAJE DE ERROR DE ACCESO Y UN BOTON DE VUELTA ATRAS!!
 		pw.println(Utilidades.bodyFiltrosyFooter());
 		pw.println(Utilidades.FIN);
 	}

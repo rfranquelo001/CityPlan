9d8ac77789f9f66b1feb0098d86a1a658bab0bfc
Raquel-Franquelo <trf.12.svb@gmail.com>
2020-05-04 15:54:38 +0200

    tests configurados

---
+++ CityPlanGit/tests/pl/TestsServletRegistro.java
@@ -0,0 +1,24 @@
+package pl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestsServletRegistro {
+
+	@Test
+	public void testServletRegistro() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDoGetHttpServletRequestHttpServletResponse() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDoPostHttpServletRequestHttpServletResponse() {
+		fail("Not yet implemented");
+	}
+
+}

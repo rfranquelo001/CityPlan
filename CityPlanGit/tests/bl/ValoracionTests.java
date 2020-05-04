784280000427c1ba7e9fb83155f65a57e0c3a98c
Raquel-Franquelo <trf.12.svb@gmail.com>
2020-05-04 17:57:25 +0200

    ultimo commit

---
+++ CityPlanGit/tests/bl/ValoracionTests.java
@@ -0,0 +1,74 @@
+package bl;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Ignore;
+
+public class ValoracionTests {
+
+	@Ignore
+	public void testValoracion() {
+		fail("Not yet implemented");
+	}
+
+	@Ignore
+	public void testGetCliente() {
+		fail("Not yet implemented");
+	}
+
+	@Ignore
+	public void testSetCliente() {
+		fail("Not yet implemented");
+	}
+
+	@Ignore
+	public void testGetEvento() {
+		fail("Not yet implemented");
+	}
+
+	@Ignore
+	public void testSetEvento() {
+		fail("Not yet implemented");
+	}
+
+	@Ignore
+	public void testGetTotalValoracion() {
+		fail("Not yet implemented");
+	}
+
+	@Ignore
+	public void testSetTotalValoracion() {
+		fail("Not yet implemented");
+	}
+
+	@Ignore
+	public void testGetRespuestasValoracion() {
+		fail("Not yet implemented");
+	}
+
+	@Ignore
+	public void testSetRespuestasValoracion() {
+		fail("Not yet implemented");
+	}
+
+	@Ignore
+	public void testCalculaValoracion() {
+		// fail("Not yet implemented");
+		System.out.println("Calcular valoracion");
+		Valoracion val = new Valoracion();
+
+		float p1 = 1;
+		float p2 = 2;
+		float p3 = 3;
+		float p4 = 4;
+		float p5 = 5;
+		float p6 = 6;
+		float p7 = 7;
+		float p8 = 8;
+
+		float result = val.calculaValoracion(p1, p2, p3, p4, p5, p6, p7, p8);
+		assertEquals();
+
+	}
+
+}

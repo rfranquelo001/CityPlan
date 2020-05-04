7b0f99a81ce8e2b7c9b469dd969a6fd8fee96366
Raquel-Franquelo <trf.12.svb@gmail.com>
2020-05-04 17:10:26 +0200

    tests null pointerException

--- CityPlanGit/tests/pl/TestsServletRegistro.java
+++ CityPlanGit/tests/pl/TestsServletRegistro.java
@@ -1,33 +1,48 @@
 package pl;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-import static org.mockito.Mockito.mock;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.junit.Ignore;
 import org.junit.Test;
+import org.mockito.Mockito;
 
-public class TestsServletRegistro {
+public class TestsServletRegistro extends Mockito {
 
-	HttpServletRequest request = mock(HttpServletRequest.class);
-	HttpServletResponse response = mock(HttpServletResponse.class);
-
-	@Test
+	@Ignore
 	public void testServletRegistro() {
 		fail("Not yet implemented");
 	}
 
-	@Test
+	@Ignore
 	public void testDoGetHttpServletRequestHttpServletResponse() {
 		fail("Not yet implemented");
 	}
 
 	@Test
-	public void testDoPostHttpServletRequestHttpServletResponse() {
+	public void testDoPostHttpServletRequestHttpServletResponse() throws ServletException, IOException {
 		// fail("Not yet implemented");
 		System.out.println("Test de recogida de datos del regisro");
+		HttpServletRequest request = mock(HttpServletRequest.class);
+		HttpServletResponse response = mock(HttpServletResponse.class);
 
+		when(request.getParameter("tipoUsuario")).thenReturn("cliente");
+		StringWriter stringWriter = new StringWriter();
+		PrintWriter writer = new PrintWriter(stringWriter);
+		when(response.getWriter()).thenReturn(writer);
+
+		new ServletRegistro().doPost(request, response);
+		verify(request, atLeast(1)).getParameter("tipoUsuario");
+		writer.flush();
+		assertTrue(stringWriter.toString().contains("My expected string"));
 	}
 
 }

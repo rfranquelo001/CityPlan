38bfbb29e9eeb5b783bf941e7a4863a3562d601b
Raquel-Franquelo <trf.12.svb@gmail.com>
2020-05-05 13:34:38 +0200

    mensaje usuario ya existe

--- CityPlanGit/src/pl/VistaResultadoRegistro.java
+++ CityPlanGit/src/pl/VistaResultadoRegistro.java
@@ -17,11 +17,27 @@ public class VistaResultadoRegistro implements InterfazVista {
 				+ "</script>";
 
 		pw.println(Utilidades.DOCTYPE);
-		pw.println(Utilidades.headConTitleStyle("Error LogIn", " "));
+		pw.println(Utilidades.headConTitleStyle("USUARIO AÑADIDO", " "));
 		pw.println(Utilidades.headerConMenu());
 		pw.println(Utilidades.scriptHead(codigoGoBack));
 		pw.println(Utilidades.headerConMenu());
 		pw.println(Utilidades.bodyContainer(userOK)); // MENSAJE DE ERROR DE ACCESO Y UN BOTON DE VUELTA ATRAS
+		pw.println(Utilidades.bodyFiltrosyFooter());
+		pw.println(Utilidades.FIN);
+	}
+
+	public void usuarioExiste(PrintWriter pw) {
+		String userExiste = "<p>EL USUARIO YA EXISTE</p><button onclick=\"goBack()\">Volver</button>";
+
+		String codigoGoBack = "<script>\n" + "function goBack() {\n" + "  window.history.back();\n" + "}\n"
+				+ "</script>";
+
+		pw.println(Utilidades.DOCTYPE);
+		pw.println(Utilidades.headConTitleStyle("USUARIO EXISTE", " "));
+		pw.println(Utilidades.headerConMenu());
+		pw.println(Utilidades.scriptHead(codigoGoBack));
+		pw.println(Utilidades.headerConMenu());
+		pw.println(Utilidades.bodyContainer(userExiste)); // MENSAJE DE ERROR DE ACCESO Y UN BOTON DE VUELTA ATRAS
 		pw.println(Utilidades.bodyFiltrosyFooter());
 		pw.println(Utilidades.FIN);
 	}

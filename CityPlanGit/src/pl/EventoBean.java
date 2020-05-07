77fc94d2960ca2d680595dc14749befd4e6a6594
Raquel-Franquelo <trf.12.svb@gmail.com>
2020-05-07 06:00:30 +0200

    He creado eventoBean para la pagina detalleEvento.xhtml

---
+++ CityPlanGit/src/pl/EventoBean.java
@@ -0,0 +1,138 @@
+package pl;
+
+import java.io.Serializable;
+import java.sql.Time;
+import java.util.Date;
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+import bl.ClaseEJB;
+import dl.Comerciante;
+import dl.Evento;
+import dl.Filtro;
+import dl.Opinion;
+
+@Named
+@RequestScoped
+public class EventoBean implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@EJB
+	private ClaseEJB ejb;
+	private Evento evento = new Evento();
+	private String codigoQR;
+	private String descripcionEvento;
+	private Date fechaEvento;
+	private Time horaEvento;
+	private String imagenEvento;
+	private String localizacionEvento;
+	private String nombreEvento;
+	private String resumenEvento;
+	private Comerciante comerciante;
+	private List<Filtro> filtros;
+	private List<Opinion> opinions;
+
+	public Evento getEvento() {
+		return evento;
+	}
+
+	public void setEvento(Evento evento) {
+		this.evento = evento;
+	}
+
+	public String getCodigoQR() {
+		return codigoQR;
+	}
+
+	public void setCodigoQR(String codigoQR) {
+		this.codigoQR = codigoQR;
+	}
+
+	public String getDescripcionEvento() {
+		return descripcionEvento;
+	}
+
+	public void setDescripcionEvento(String descripcionEvento) {
+		this.descripcionEvento = descripcionEvento;
+	}
+
+	public Date getFechaEvento() {
+		return fechaEvento;
+	}
+
+	public void setFechaEvento(Date fechaEvento) {
+		this.fechaEvento = fechaEvento;
+	}
+
+	public Time getHoraEvento() {
+		return horaEvento;
+	}
+
+	public void setHoraEvento(Time horaEvento) {
+		this.horaEvento = horaEvento;
+	}
+
+	public String getImagenEvento() {
+		return imagenEvento;
+	}
+
+	public void setImagenEvento(String imagenEvento) {
+		this.imagenEvento = imagenEvento;
+	}
+
+	public String getLocalizacionEvento() {
+		return localizacionEvento;
+	}
+
+	public void setLocalizacionEvento(String localizacionEvento) {
+		this.localizacionEvento = localizacionEvento;
+	}
+
+	public String getNombreEvento() {
+		return nombreEvento;
+	}
+
+	public void setNombreEvento(String nombreEvento) {
+		this.nombreEvento = nombreEvento;
+	}
+
+	public String getResumenEvento() {
+		return resumenEvento;
+	}
+
+	public void setResumenEvento(String resumenEvento) {
+		this.resumenEvento = resumenEvento;
+	}
+
+	public Comerciante getComerciante() {
+		return comerciante;
+	}
+
+	public void setComerciante(Comerciante comerciante) {
+		this.comerciante = comerciante;
+	}
+
+	public List<Filtro> getFiltros() {
+		return filtros;
+	}
+
+	public void setFiltros(List<Filtro> filtros) {
+		this.filtros = filtros;
+	}
+
+	public List<Opinion> getOpinions() {
+		return opinions;
+	}
+
+	public void setOpinions(List<Opinion> opinions) {
+		this.opinions = opinions;
+	}
+
+}

ea1db46cf5caaec096ab626fd919f2b9119d2262
Raquel-Franquelo <trf.12.svb@gmail.com>
2020-05-08 13:41:21 +0200

    añadida Query para obtener lista de nombres de clientes

--- CityPlanGit/src/dl/Cliente.java
+++ CityPlanGit/src/dl/Cliente.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 
@@ -15,7 +16,9 @@ import javax.persistence.OneToMany;
  * 
  */
 @Entity
-@NamedQuery(name = "Cliente.findAll", query = "SELECT c FROM Cliente c")
+@NamedQueries({ @NamedQuery(name = "Cliente.findAll", query = "SELECT c FROM Cliente c"),
+		@NamedQuery(name = "Cliente.findName", query = "SELECT c.nombreCliente FROM Cliente c") })
+
 //METER QUERY EN FUNCION DEL NOMBRE
 public class Cliente implements Serializable {
 	private static final long serialVersionUID = 1L;
